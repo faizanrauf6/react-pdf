33d0d8adc70111637be028dcd748c96db729602b
faizan-purelogics <122025996+faizan-purelogics@users.noreply.github.com>
2023-11-17 16:09:13 +0500

    Simple html pdf viewer

--- src/components/PdfViewer.jsx
+++ src/components/PdfViewer.jsx
@@ -1,47 +1,19 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
-import { pdfjs } from "react-pdf";
-// pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import React from "react";
 
 const PdfViewer = () => {
-  const [numPages, setNumPages] = useState(null);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  const onDocumentLoadSuccess = ({ numPages }) => {
-    setNumPages(numPages);
-  };
-
-  const handleNextPage = () => {
-    if (pageNumber < numPages) {
-      setPageNumber(pageNumber + 1);
-    }
-  };
-
-  const handlePrevPage = () => {
-    if (pageNumber > 1) {
-      setPageNumber(pageNumber - 1);
-    }
-  };
+  const pdfUrl = "/20231115061930-Healt.pdf";
 
   return (
     <div>
-      <Document
-        file="/20231115061930-Healt.pdf"
-        onLoadSuccess={onDocumentLoadSuccess}
-      >
-        <Page
-          key={pageNumber}
-          pageNumber={pageNumber}
-          renderTextLayer={false}
-          renderAnnotationLayer={false}
-        />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-      <button onClick={handlePrevPage}>Previous</button>
-      <button onClick={handleNextPage}>Next</button>
+      <center>
+        <object
+          data={pdfUrl}
+          type="application/pdf"
+          width="800"
+          height="700"
+        ></object>
+      </center>
     </div>
   );
 };
